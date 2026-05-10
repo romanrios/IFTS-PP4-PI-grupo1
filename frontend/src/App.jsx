c1b53cbc1fa2da2e1038169059666dc9fc75d0c5
Román Ríos <romanrios@live.com>
2026-05-10 01:14:07 -0300

    vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
 import { useAuth } from "./context/AuthContext";
-
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
 import ProtectedRoute from "./components/ProtectedRoute";
-
 import AdminDashboard from "./pages/AdminMenuPage";
 import AdoptantesPage from "./pages/AdoptantesPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -17,50 +13,14 @@ function App() {
 
   return (
     <BrowserRouter>
-      {/* Header solo si está logueado */}
       {user && <Header />}
-
       <main>
-
         <Routes>
-          {/* LOGIN */}
           <Route path="/login" element={<LoginPage />} />
-
-          {/* ADMIN */}
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute adminOnly>
-                <AdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* MICHIS */}
-          <Route
-            path="/michis"
-            element={
-              <ProtectedRoute>
-                <MichisPage />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* ADOPTANTES */}
-          <Route
-            path="/adoptantes"
-            element={
-              <ProtectedRoute adminOnly>
-                <AdoptantesPage />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* REDIRECCIÓN */}
-          <Route
-            path="*"
-            element={<Navigate to={user ? "/michis" : "/login"} />}
-          />
+          <Route path="/admin" element={<ProtectedRoute adminOnly> <AdminDashboard /> </ProtectedRoute>} />
+          <Route path="/michis" element={<ProtectedRoute> <MichisPage /> </ProtectedRoute>} />
+          <Route path="/adoptantes" element={<ProtectedRoute adminOnly> <AdoptantesPage /> </ProtectedRoute>} />
+          <Route path="*" element={<Navigate to={user ? "/michis" : "/login"} />} />
         </Routes>
       </main>
       {user && <Footer />}
